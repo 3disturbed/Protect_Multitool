7e62b1ddeb41d92f5e4afe22d728f68cf72cee1e
farhatamanna islam <farha.islam@students.codeinstitute.net>
2025-01-16 19:41:21 +0000

    Remove: twice definition in account_home views and Add: account_home urls in project urls

--- protect_multitool/urls.py
+++ protect_multitool/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('account_home/', include('account_home.urls')),
 ]
