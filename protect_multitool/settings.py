55924a64192aa266f1984e70e377f5e9e65aa1fa
Farha Tamanna Islam <farha@farhas-air.speedport.ip>
2025-01-16 19:52:38 +0100

    Fix: two times adding of account_home

--- protect_multitool/settings.py
+++ protect_multitool/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account_home',
     'django.contrib.auth',
     'allauth',
     'allauth.account',
