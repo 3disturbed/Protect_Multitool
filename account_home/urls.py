80497f7ba02044211370a6f1229baafe95bc14a6
farhatamanna islam <farha.islam@students.codeinstitute.net>
2025-01-17 18:23:36 +0000

    Fix: emergency contact urls to make it reachable from homepage

--- account_home/urls.py
+++ account_home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'account_home' 
+
 urlpatterns = [
     path('emergencycontact/', views.emergency_contact_list, name='emergency_contact_list'),
     path('add-emergencycontact/', views.add_emergency_contact, name='add_emergency_contact'),
