cec61049dd5658c41e977c1849050c6834c2d908
Michael Mc Court <mmccourt928@outlook.com>
2025-01-18 08:51:48 +0000

    route planner model and form added

--- route_planner/urls.py
+++ route_planner/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'route_planner'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # This will redirect to your main route planner page
-    path('planner/', views.planner, name='planner'),  # Your actual route planner page
+   path('', views.home, name='home'),  # This will redirect to your main route planner page,
+   path('planner/', views.planner, name='planner'),  # Your actual route planner page
 ]
--- route_planner/views.py
+++ route_planner/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from .forms import PlannedRouteForm
+from .models import PlannedRoute
 
 def home(request):
     
     return redirect('route_planner:planner')
 
+@login_required
 def planner(request):
-    
-    return render(request, 'route_planner/planner.html')
+    if request.method == 'POST':
+        form = PlannedRouteForm(request.POST)
+        if form.is_valid():
+            planned_route = form.save(commit=False)
+            planned_route.user = request.user
+            planned_route.save()
+            return redirect('route_planner:planner')
+    else:
+        form = PlannedRouteForm()
+
+    return render(request, 'route_planner/planner.html', {'form': form})
