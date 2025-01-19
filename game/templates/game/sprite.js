03d49017034358852badee90ca46e34ad3cb2c96
Darklington <codemonkey@3disturbedmonkeys.co.uk>
2025-01-19 17:21:39 +0000

    Add: Simple sprite class, sprite.js

---
+++ game/templates/game/sprite.js
@@ -0,0 +1,67 @@
+
+// Sprite class for creating and rendering sprites
+export default class Sprite {
+    constructor({ imageSrc, frameWidth, frameHeight, frames, frameDelay, scaleFactor, opacity, rotation, yFrame, x, y, width, height, velocity, boyancy, jump, alive, GullDistance }) {
+        this.image = new Image();
+        this.loaded = false;
+        this.image.onload = () => {
+            this.loaded = true;
+        };
+        this.image.src = imageSrc;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.frames = frames;
+        this.frameDelay = frameDelay;
+        this.scaleFactor = scaleFactor;
+        this.opacity = opacity;
+        this.rotation = rotation;
+        this.yFrame = yFrame;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.velocity = velocity;
+        this.boyancy = boyancy;
+        this.jump = jump;
+        this.alive = alive;
+        this.GullDistance = GullDistance;
+        this.currentFrame = 0;
+        this.frameTimer = 0;
+    }
+
+    update(deltaTime) {
+        if (!deltaTime) return;
+        
+        this.frameTimer += deltaTime;
+        if (this.frameTimer >= this.frameDelay) {
+            this.currentFrame = (this.currentFrame + 1) % this.frames;
+            this.frameTimer = 0;
+        }
+    }
+
+    draw(context) {
+        if (!this.loaded) return;
+        
+        context.save();
+        context.globalAlpha = this.opacity;
+        context.translate(this.x + this.width / 2, this.y + this.height / 2);
+        context.rotate((this.rotation * Math.PI) / 180);
+        context.drawImage(
+            this.image,
+            this.currentFrame * this.frameWidth,
+            this.yFrame * this.frameHeight,
+            this.frameWidth,
+            this.frameHeight,
+            -this.width / 2,
+            -this.height / 2,
+            this.width * this.scaleFactor,
+            this.height * this.scaleFactor
+        );
+        context.restore();
+    }
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
