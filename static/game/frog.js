6525882fa91cb7b9bbe7dcc6a00152941fb45a5d
Darklington <codemonkey@3disturbedmonkeys.co.uk>
2025-01-19 21:49:40 +0000

    fix Game Relative paths

--- static/game/frog.js
+++ static/game/frog.js
@@ -1,5 +1,5 @@
-import Sprite from '../../../game/sprite.js';
-import { FROG_CONFIG } from '../../../game/constants.js';
+import Sprite from './sprite.js';
+import { FROG_CONFIG } from './constants.js';
 
 export const frog = new Sprite({
     ...FROG_CONFIG,
