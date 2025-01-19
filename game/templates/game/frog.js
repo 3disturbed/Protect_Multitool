f40902e8de0275754a4c84111e59b4dd8d75f8b4
Darklington <codemonkey@3disturbedmonkeys.co.uk>
2025-01-19 21:01:35 +0000

    Moved Static scripts

--- game/templates/game/frog.js
+++
@@ -1,8 +0,0 @@
-import Sprite from '../../../../game/sprite.js';
-import { FROG_CONFIG } from '../../../../game/constants.js';
-
-export const frog = new Sprite({
-    ...FROG_CONFIG,
-    x: 50,
-    y: window.innerHeight / 2
-});
---
+++ static/game/frog.js
@@ -0,0 +1,8 @@
+import Sprite from '../../../game/sprite.js';
+import { FROG_CONFIG } from '../../../game/constants.js';
+
+export const frog = new Sprite({
+    ...FROG_CONFIG,
+    x: 50,
+    y: window.innerHeight / 2
+});
