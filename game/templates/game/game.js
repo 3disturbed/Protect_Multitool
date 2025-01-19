1c684aa27a0a93eab2fc229711bee5a32404e347
Darklington <codemonkey@3disturbedmonkeys.co.uk>
2025-01-19 17:23:55 +0000

    edit: game.js, Handle imports

--- game/templates/game/game.js
+++ game/templates/game/game.js
@@ -1,1 +1,16 @@
 // Game loop goes here
+import Sprite from './sprite.js';
+import { frog } from '../Protect_Multitool/game/templates/game/frog.js';
+import {
+    SCROLL_SPEED,
+    SURFACE_SCROLL_SPEED,
+    WALL_SPACE,
+    WALL_SPEED,
+    WALL_SPAWN_INTERVAL,
+    FLOOR_HEIGHT,
+    SURFACE_HEIGHT,
+    FRAME_WIDTH,
+    KICK_COOLDOWN,
+    FLOOR_SPRITE_CONFIG,
+    // ... import other constants
+} from './constants.js';
